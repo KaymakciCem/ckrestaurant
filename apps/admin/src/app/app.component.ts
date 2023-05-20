6f7fea3eaea252645d43b3d887bae2a73d0060da
cem kaymakci <cemkaymakci@gmail.com>
2023-05-20 14:51:49 +0200

    bootstrap added

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'admin-root',
+  selector: 'ckrestaurant-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
