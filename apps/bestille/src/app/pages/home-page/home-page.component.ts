b1475f933fff8a03903ae350fc75c84c6c33be92
cem kaymakci <cemkaymakci@gmail.com>
2023-05-20 16:31:30 +0200

    admin panel creation

--- apps/bestille/src/app/pages/home-page/home-page.component.ts
+++ apps/bestille/src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ckrestaurant-home-page',
+  selector: 'bestille-home-page',
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {}
